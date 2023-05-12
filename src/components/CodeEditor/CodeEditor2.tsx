352301059cf151b82e6dcdfd443d7119e0a18c2b
MatthewCaseres <matthewcaseres@outlook.com>
2023-05-11 22:05:04 -0500

    more tabs work

--- src/components/CodeEditor/CodeEditor2.tsx
+++ src/components/CodeEditor/CodeEditor2.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react'
+import { produce } from 'immer'
+import { TabsProvider, useTabs } from './EditorContext'
 
 import { usePython } from 'react-py'
 import type { Packages } from 'react-py/dist/types/Packages'
+import type { Tab } from './EditorContext'
 
 import Controls from './Controls'
 import { ArrowUpIcon, PlayIcon, StopIcon } from '@heroicons/react/solid'
@@ -12,44 +15,27 @@ import 'ace-builds/src-noconflict/theme-textmate'
 import 'ace-builds/src-noconflict/theme-idle_fingers'
 import 'ace-builds/src-noconflict/ext-language_tools'
 
-import { Provider, atom, useAtom } from 'jotai'
 import PythonSVG from './PythonSVG'
 
-const tabIndexAtom = atom(0)
-
-type Tab = {
-  title: string
-  content: string
-}
-
 function Tabs() {
-  const [tabIndex, setTabIndex] = useAtom(tabIndexAtom)
-  const tabs: Tab[] = [
-    {
-      title: 'file1.py',
-      content: 'print("hello world")',
-    },
-    {
-      title: 'file2.py',
-      content: 'print("hello world")',
-    },
-    {
-      title: 'f.py',
-      content: 'print("hello world")',
-    },
-  ]
+  const {
+    state: { tabIndex, tabs },
+    dispatch,
+  } = useTabs()
 
   return (
     <div className="">
-      <div className="flex text-xs font-semibold text-neutral-400">
+      <div className="flex text-xs font-semibold ">
         {tabs.map((tab, i) => (
           <div
             key={i}
-            className={`flex items-center ${
+            className={`flex select-none items-center ${
               i === tabs.length - 1 ? '' : 'border-r'
+            } ${
+              i === tabIndex ? 'text-neutral-200' : 'text-neutral-400'
             } border-black bg-neutral-900 p-2 text-center`}
             style={{ backgroundColor: i == tabIndex ? '#323232' : '' }}
-            onClick={() => setTabIndex(i)}
+            onClick={() => dispatch({ type: 'SET_TAB_INDEX', payload: i })}
           >
             <PythonSVG />
             <span>{tab.title}</span>
@@ -73,19 +59,31 @@ const editorOnLoad = (editor) => {
 }
 
 interface CodeEditorProps {
-  code: string
+  tabs: Tab[]
   packages?: Packages
 }
 
-export default function CodeEditor(props: CodeEditorProps) {
-  const { code, packages } = props
-  const [input, setInput] = useState(code.trimEnd())
+export default function CodeEditor2(props: CodeEditorProps) {
+  return (
+    <TabsProvider tabs={props.tabs}>
+      <CodeEditor {...props} />
+    </TabsProvider>
+  )
+}
+
+function CodeEditor(props: { packages?: Packages }) {
+  const {
+    state: { tabIndex, tabs },
+    initialState,
+    dispatch,
+  } = useTabs()
+  const { packages } = props
   const [showOutput, setShowOutput] = useState(false)
 
-  useEffect(() => {
-    setInput(code.trimEnd())
-    setShowOutput(false)
-  }, [code])
+  // useEffect(() => {
+  //   dispatch({ type: 'SET_TABS', payload: initialTabs })
+  //   setShowOutput(false)
+  // }, [initialTabs])
 
   const {
     runPython,
@@ -97,7 +95,7 @@ export default function CodeEditor(props: CodeEditorProps) {
   } = usePython({ packages })
 
   function run() {
-    runPython(input)
+    runPython(tabs[tabIndex].content)
     setShowOutput(true)
   }
 
@@ -108,12 +106,13 @@ export default function CodeEditor(props: CodeEditorProps) {
 
   function reset() {
     setShowOutput(false)
-    setInput(code.trimEnd())
+    dispatch({ type: 'RESET_TABS', payload: initialState })
   }
 
   return (
     <div className="relative mb-10 flex flex-col">
-      {/* <Controls
+      <Tabs />
+      <Controls
         items={[
           {
             label: 'Run',
@@ -130,24 +129,29 @@ export default function CodeEditor(props: CodeEditorProps) {
             disabled: isRunning,
           },
         ]}
-      /> */}
-      <Tabs />
+      />
 
       <AceEditor
-        value={input}
+        value={tabs.length > 0 ? tabs[tabIndex].content : ''}
         mode="python"
         name="CodeBlock"
         fontSize="0.9rem"
         className="-mt-1 min-h-[4rem] overflow-clip rounded shadow-md"
         theme="idle_fingers"
-        onChange={(newValue) => setInput(newValue)}
+        onChange={(newValue) =>
+          dispatch({
+            type: 'SET_TABS',
+            payload: produce(tabs, (draft) => {
+              draft[tabIndex].content = newValue
+            }),
+          })
+        }
         width="100%"
         maxLines={Infinity}
         onLoad={editorOnLoad}
         editorProps={{ $blockScrolling: true }}
         setOptions={editorOptions}
       />
-
       {showOutput && (
         <pre className=" overflow-auto text-left">
           <code>{stdout}</code>
--- src/pages/render.tsx
+++ src/pages/render.tsx
@@ -10,6 +10,21 @@ const CodeEditor = dynamic(
 )
 // import CodeEditor from '@/components/CodeEditor/CodeEditor2'
 
+const initialTabs = [
+  {
+    title: 'file1.py',
+    content: 'print("hello world1")',
+  },
+  {
+    title: 'file2.py',
+    content: 'print("hello world2")',
+  },
+  {
+    title: 'f.py',
+    content: 'print("hello world")',
+  },
+]
+
 function App() {
   const initialTabTitles = ['file1.py', 'file2.py', 'C']
 
@@ -19,7 +34,7 @@ function App() {
         ds sfd fs sadf sdf sfd sdfa fd dsfa afds dfsa safd fsda dsaf dsfa sadf
         fsda fdsa dsfa fsad dsaf df
       </div>
-      <CodeEditor code="Hello world" />
+      <CodeEditor tabs={initialTabs} />
     </SidebarLayout>
   )
 }
