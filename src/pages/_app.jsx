3116159e95b1201cb2466a9432dcd8a53e2146d8
MatthewCaseres <matthewcaseres@outlook.com>
2023-05-13 13:01:38 -0500

    Give each editor their own PythonProvider

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -31,7 +31,6 @@ export default function App({ Component, pageProps, router }) {
 
   return (
     <>
-    <PythonProvider lazy={true} terminateOnCompletion={true}>
       <div className="fixed inset-0 flex justify-center sm:px-8">
         <div className="flex w-full max-w-7xl lg:px-8">
           <div className="w-full" />
@@ -44,7 +43,6 @@ export default function App({ Component, pageProps, router }) {
           <Analytics />
         </main>
       </div>
-    </PythonProvider>
     </>
   )
 }
