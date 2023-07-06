c9d05f000441511787ff3e04db9867d7b81fccec
MatthewCaseres <matthewcaseres@outlook.com>
2023-07-05 19:26:38 -0500

    surprised I shipped code without checking this.

--- src/components/WhyOpenSource.jsx
+++ src/components/WhyOpenSource.jsx
@@ -3,22 +3,22 @@ import { useIntl, FormattedMessage } from 'react-intl'
 export function WhyOpenSource() {
   const intl = useIntl()
 
-  return <div className="text-zinc-800 dark:text-zinc-100">
-    <div className="mx-auto max-w-7xl divide-y-2 divide-gray-200 py-10 px-4 sm:py-16 sm:px-6 lg:px-8 prose">
-      <h2 className="text-3xl font-extrabold">
-      <FormattedMessage id={"why?"} />
+  return (
+    <div className="prose  mx-auto max-w-7xl divide-y-2 divide-gray-200 py-10 px-4 sm:py-16 sm:px-6 lg:px-8">
+      <h2 className="text-3xl font-extrabold text-zinc-800 dark:text-zinc-100 ">
+        <FormattedMessage id={'why?'} />
       </h2>
-      <ol className="text-lg">
+      <ol className="text-lg text-zinc-800 dark:text-zinc-100">
         <li>
-        <FormattedMessage id={"ans1"} />
+          <FormattedMessage id={'ans1'} />
         </li>
         <li>
-        <FormattedMessage id={"ans2"} />
+          <FormattedMessage id={'ans2'} />
         </li>
         <li>
-        <FormattedMessage id={"ans3"} />
+          <FormattedMessage id={'ans3'} />
         </li>
       </ol>
     </div>
-  </div>
+  )
 }
