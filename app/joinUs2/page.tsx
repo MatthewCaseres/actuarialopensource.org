50605b9d19bf60a8a015e929957f2c043c9cf65e
MatthewCaseres <matthewcaseres@outlook.com>
2023-07-09 05:31:29 -0500

    upgrade next.js

---
+++ app/joinUs2/page.tsx
@@ -0,0 +1,23 @@
+export default async function Page() {
+  return (
+    <div>
+      <h1>lol</h1>
+      <h1>lol</h1>
+      <h1>lol</h1>
+      <h1>lol</h1>
+      <h1>lol</h1>
+      <h1>lol</h1>
+      <h1>lol</h1>
+      <h1>lol</h1>
+      <h1>lol</h1>
+      <h1>lol</h1>
+      <h1>lol</h1>
+      <h1>lol</h1>
+      <h1>lol</h1>
+      <h1>lol</h1>
+      <h1>lol</h1>
+      <h1>lol</h1>
+      <h1>lol</h1>
+    </div>
+  )
+}
